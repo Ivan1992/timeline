484c543305db984b85c521291287b9c26a96b6aa
process4 <info@process4.biz>
2019-06-25 15:12:57 +0200

    initial added

--- initial.js
+++ initial.js
@@ -2,7 +2,10 @@
     var showSectionTitles = true;
 
     const http = new XMLHttpRequest();
-    const url = "https://cdn.contentful.com/spaces/urwi2cb4sy5x/environments/master/entries?access_token=qcarnwXClkOkzzaM5E4miBW1__07GyU_GNSuuDEATM&include=10&content_type=section";
+    const tok = "qcarnwXClkOkzzaM5E4miBW1";
+    const tok2 = "07GyU_GNSuuDEATM";
+    const spc = "urwi2cb4sy5x";
+    const url = `https://cdn.contentful.com/spaces/${spc}/environments/master/entries?access_token=${tok}__${tok2}&include=10&content_type=section`;
 
     var timelineData = [];
     http.onreadystatechange = function() {
@@ -18,10 +21,21 @@
             timeline.setData(arr);
             timeline.onDataLoad(arr);
             timeline.draw();
-            $("a.modals").each( function(i,obj) { 
+
+
+            arr.forEach((section, index) => {
+                //if (section.items.filter(x => x.fulltext).length > 0) {
+                var div = $("<div>", { 'id': `section${index}`, "class": "modalWrapper" });
+                div.html(`<div class="close-section${index} closeDiv"></div><div class="modal-content"></div>`).prependTo("body");
+                $(`.section${index}Link`).animatedModal({ color: section.color });
+                div.css("background-color", "#bbb");
+                div.css("background-image", `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='52' height='52' viewBox='0 0 52 52'%3E%3Cpath fill='%23${section.color.slice(1)}' fill-opacity='0.1' d='M0 17.83V0h17.83a3 3 0 0 1-5.66 2H5.9A5 5 0 0 1 2 5.9v6.27a3 3 0 0 1-2 5.66zm0 18.34a3 3 0 0 1 2 5.66v6.27A5 5 0 0 1 5.9 52h6.27a3 3 0 0 1 5.66 0H0V36.17zM36.17 52a3 3 0 0 1 5.66 0h6.27a5 5 0 0 1 3.9-3.9v-6.27a3 3 0 0 1 0-5.66V52H36.17zM0 31.93v-9.78a5 5 0 0 1 3.8.72l4.43-4.43a3 3 0 1 1 1.42 1.41L5.2 24.28a5 5 0 0 1 0 5.52l4.44 4.43a3 3 0 1 1-1.42 1.42L3.8 31.2a5 5 0 0 1-3.8.72zm52-14.1a3 3 0 0 1 0-5.66V5.9A5 5 0 0 1 48.1 2h-6.27a3 3 0 0 1-5.66-2H52v17.83zm0 14.1a4.97 4.97 0 0 1-1.72-.72l-4.43 4.44a3 3 0 1 1-1.41-1.42l4.43-4.43a5 5 0 0 1 0-5.52l-4.43-4.43a3 3 0 1 1 1.41-1.41l4.43 4.43c.53-.35 1.12-.6 1.72-.72v9.78zM22.15 0h9.78a5 5 0 0 1-.72 3.8l4.44 4.43a3 3 0 1 1-1.42 1.42L29.8 5.2a5 5 0 0 1-5.52 0l-4.43 4.44a3 3 0 1 1-1.41-1.42l4.43-4.43a5 5 0 0 1-.72-3.8zm0 52c.13-.6.37-1.19.72-1.72l-4.43-4.43a3 3 0 1 1 1.41-1.41l4.43 4.43a5 5 0 0 1 5.52 0l4.43-4.43a3 3 0 1 1 1.42 1.41l-4.44 4.43c.36.53.6 1.12.72 1.72h-9.78zm9.75-24a5 5 0 0 1-3.9 3.9v6.27a3 3 0 1 1-2 0V31.9a5 5 0 0 1-3.9-3.9h-6.27a3 3 0 1 1 0-2h6.27a5 5 0 0 1 3.9-3.9v-6.27a3 3 0 1 1 2 0v6.27a5 5 0 0 1 3.9 3.9h6.27a3 3 0 1 1 0 2H31.9z'%3E%3C/path%3E%3C/svg%3E")`);
+                //}
+            });
+            /* $("a.modals").each( function(i,obj) { 
                 var color = $(this).parents("div.section").find("div.section-bg").css("backgroundColor");
                 $(this).animatedModal({color: color});
-            });
+            }); */
         }
     }
 
